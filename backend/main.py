21361e9e439e97d0e27689365d6a4ef011c188e2
maklakandr <maklakandr@myvuw.ac.nz>
2025-10-03 19:56:19 +1300

    extended main.py to have image upload

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import shutil
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello"}
+    return {"message": "Hello backend"}
+
+@app.post("/uploadfile/")
+async def upload_image(file: UploadFile = File(...)):
+    file_location = f"temp_{file.filename}"
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"info": f"file '{file.filename}' saved at '{file_location}'"}
